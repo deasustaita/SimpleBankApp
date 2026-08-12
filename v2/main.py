b6b97ea29aafcbd4f09084a1e1cde36f63309c21
deasustaita@gmail.com <deasustaita@gmail.com>
2026-08-12 00:40:56 -0400

    added router link to main.py

--- v2/main.py
+++ v2/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI, HTTPException, status
 from fastapi.responses import JSONResponse
 
-from routers.CustomerRouter import router
+from routers.CustomerRouter import router as crouter
+from routers.AccountRouter import router as arouter
+
+
 app = FastAPI()
 
-app.include_router(router, prefix="/api/v1/customers")
+app.include_router(crouter, prefix="/api/v1/customers")
+app.include_router(arouter, prefix="/api/v1/accounts")
 
 # post create customer profile
 
